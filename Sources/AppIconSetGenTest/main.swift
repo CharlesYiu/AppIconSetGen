d50e8d76e7422bb3777fa9d3644968fa8416e258
Charles Yiu <me@yiu.ch>
2023-11-08 17:21:08 +0800

    Improve test

--- Sources/AppIconSetGenTest/main.swift
+++ Sources/AppIconSetGenTest/main.swift
@@ -6,11 +6,21 @@
 //
 
 import Foundation
-import AppIconSetGen
 import CoreImage
+import AppIconSetGen
+
+// A solid blue square with dimensions of 200x200
+let blueSquare = CIImage(color: .blue).cropped(to: .init(x: 0, y: 0, width: 200, height: 200))
+
+// Would export for iOS, watchOS, and macOS
+let iconTypes = [AppIconSetType.iOS, AppIconSetType.watchOS, AppIconSetType.macOS]
+
+// Would export to ~/blue/blue.appiconset
+let outputName = "blue"
+let outputDir = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("blue")
 
 do {
-    try generateAppIconSet(image: CIImage(color: .blue).cropped(to: .init(x: 0, y: 0, width: 200, height: 300)), types: [.iOS, .watchOS, .macOS], name: "Blue", output: .init(string: "file:///Users/charlesyiu/blue")!)
+    try generateAppIconSet(image: blueSquare, types: iconTypes, name: outputName, output: outputDir)
 } catch {
     print(error)
 }
